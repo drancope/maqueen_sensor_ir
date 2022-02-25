4ed8d76dec037fec8c7b29b2afbc1539ecfab56d
Antonio Contreras <drancope@gmail.com>
2022-02-25 16:45:50 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 basic.forever(function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 30)
         basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        basic.pause(1000)
     }
 })
