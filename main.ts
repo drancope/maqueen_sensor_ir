ed5dbe244f760d51e06728c5f5f9f72dd2b46ce2
Antonio Contreras <drancope@gmail.com>
2022-02-25 16:43:33 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        basic.pause(500)
+    }
+})
